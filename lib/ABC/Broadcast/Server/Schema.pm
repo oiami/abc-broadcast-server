d7e5c7eb78b3d73c854d51ae9ccbb2d9439e6fde
OiOiOiOi <oi@abctech-thailand.com>
2013-12-24 17:38:48 +0700

    Make database to be avialable to use

--- lib/ABC/Broadcast/Server/Schema.pm
+++ lib/ABC/Broadcast/Server/Schema.pm
@@ -1,24 +1,20 @@
+use utf8;
 package ABC::Broadcast::Server::Schema;
 
-use utf8;
-use Moose;
-use MooseX::MarkAsMethods autoclean => 1;
-extends 'DBIx::Class::Schema';
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
 
-our $VERSION = '0.01';
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Schema';
 
 __PACKAGE__->load_namespaces;
 
-=head1 AUTHORS
 
-yo
+# Created by DBIx::Class::Schema::Loader v0.07035 @ 2013-12-24 16:48:13
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:VgZ5EPnhvpCME7Lc3al9xA
 
-=head1 COPYRIGHT
 
-All rights reserved.
-
-=cut
-
-__PACKAGE__->meta->make_immutable;
-
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
